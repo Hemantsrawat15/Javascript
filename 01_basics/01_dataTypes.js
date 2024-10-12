174bf551996d10d8a2dd2e68a040b3e8cd43098d
Hemant Singh Rawat <112156298+Hemantsrawat15@users.noreply.github.com>
2024-10-12 12:01:52 +0000

    edit

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //as soon as we write this statement the whole JS code will be treated as newer version
+
+//alert(3+3) // we are using node js not browser
+
+let name ="Hemant";
+let age= 18;
+let isLoggedIn= true;
+
+/*
+number
+bigInt
+string
+boolean
+null=> standalone Value.  console.log(typeof null) => object
+undefined => abhi value assign nahi hua hai. console.log(typeof undefined) => undefined
+symbol => unique
+
+OBJECT
+*/
+console.log(typeof undefined);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "Hemant@gmail.com"; //use let more than var
+var accountPassword = "12345";
+accountCity= "Jaipur"; //allowed in javascript but not at all recommended
+let accountState;
+
+// accountId=2; not allowed
+accountEmail="Hello@gmail.com";
+accountPassword="12345678";
+accountCity="Bengaluru";
+
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState]);
