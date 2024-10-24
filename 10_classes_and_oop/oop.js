08636c21755d3f8df4c84e49d367aad25e6427f0
Hemant Singh Rawat <112156298+Hemantsrawat15@users.noreply.github.com>
2024-10-24 10:54:33 +0000

    oops

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -10,3 +10,23 @@ const user = {
 }
 console.log(user.username);
 console.log(user.getUserDetails())
+
+function User(username , loginCount, isLoggedIn){
+    this.username= username;
+    this.loginCount= loginCount;
+    this.isLoggedIn= isLoggedIn;
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+    }
+    return this;
+}
+
+// const user1 = User("Hemant", 10, true);
+// const user2 = User("Pokemon", 20, false); // the values of user1 get altered
+// console.log(user1);
+
+
+const user1 = new User("Hemant", 10, true);
+const user2 = new User("Pokemon", 20, false); // the values of user1 get altered
+console.log(user1);
+console.log(user2);
