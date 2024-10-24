73f35a263b4f3e589d950cf11dc5786f85d1e0dd
Hemant Singh Rawat <112156298+Hemantsrawat15@users.noreply.github.com>
2024-10-24 10:15:58 +0000

    devcontainer

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,12 @@
+// Object literal
+const user = {
+    username : "Hemant",
+    loginCount: 8,
+    signedIn: true,
+    getUserDetails : function(){
+        console.log("Got user details from database");
+        console.log(`Username : ${this.username}`);
+    }
+}
+console.log(user.username);
+console.log(user.getUserDetails())
